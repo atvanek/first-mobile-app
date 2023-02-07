4f91c9029a23e0811898091d105e19a7a1945dcd
atvanek <99850013+atvanek@users.noreply.github.com>
2023-02-07 12:05:28 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	Image,
+	View,
+	Button,
+	ScrollView,
+	SafeAreaView,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Image
+				source={{
+					uri: "https://assets.stickpng.com/images/5a4e432a2da5ad73df7efe7a.png",
+				}}
+				style={styles.loginLogo}></Image>
+			<TextInput style={styles.input} name='username' placeholder='username' />
+			<TextInput style={styles.input} placeholder='password' />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	input: {
+		width: 90 + "%",
+		padding: 10,
+		margin: 2,
+		backgroundColor: "#FAFAFA",
+	},
+
+	loginLogo: {
+		height: 70,
+		width: 50 + "%",
+	},
 });
